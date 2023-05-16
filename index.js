d4f00067d2c3734e4eab7c0478808b7258045d96
Marcos <97557948+Kamurskol@users.noreply.github.com>
2023-05-16 10:51:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+qrInput = container.querySelector('.form input');
+generateBtn = container.querySelector('.form button');
+qrImg = container.querySelector('.qr-code .img');
+
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue){
+        alert('Insire ua URL para gerar um Qr Code.')
+        return;
+    }
+    generateBtn.innerText = "Gerando um Qr Code...";
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=" + qrValue;
+
+    qrImg.addEventListener('load', ()=>{
+        generateBtn.innerText = "Gerar Qr Code";
+        container.classList.add('active');
+    });
+});
+
+qrInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        generateBtn.click();
+    }
+});
+
+qrInput.addEventListener('keyup', ()=>{
+    if(!qrInput.value){
+        container.classList.remove('active');
+    };
+
+}); 
